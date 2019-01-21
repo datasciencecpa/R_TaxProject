fdf08db55d5050c8162c1d17d73700a301dfb592
longnguyen87 <nguyenhlongvn@gmail.com>
2019-01-20 22:07:07 -0600

    Adding Information Summary Tab

--- app.R
+++ app.R
@@ -3,7 +3,7 @@
 # Date: 01/19/2019
 library(shiny)
 library(shinyjs) #loading addional package to enable more UI experience
-
+library(DT)
 ui <- fluidPage(
   useShinyjs(),
   titlePanel("Federal Income Tax 2018 & 2017"),
@@ -20,16 +20,16 @@ ui <- fluidPage(
     tabPanel("Help", 
         navlistPanel("General Questions",
           tabPanel("Filing Status",
-                   "1.IRS Interactive Tax Assistant: ", a(href = "https://www.irs.gov/help/ita/what-is-my-filing-status", "What is my filing status?"
+                   "1.IRS Interactive Tax Assistant: ", a(href = "https://www.irs.gov/help/ita/what-is-my-filing-status", "What is my filing status?")
           )
-        )
-             
-             
-    )
-    #tabPanel("About",
-      
+        )         
+    ),
+    tabPanel("Information Summary",
+      navlistPanel("Information you Enter:",
+        tabPanel("Filing Status & Dependent Infor You Entered:",
+                 dataTableOutput("FS_Summary"))                     
+      )
     )       
-             
              
   )
   
@@ -37,6 +37,9 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   filingStatus <- callModule(filingStatus, "filingStatus")
+  output$FS_Summary <- renderDataTable({
+    filingStatus()
+  })
 }
 
 shinyApp(ui, server)
--- filingStatus.R
+++ filingStatus.R
@@ -1,117 +1,117 @@
 filingStatusUI <- function (id){
   ns <- NS(id)
   tabPanel("Filing Status & Dependency",
-           fluidRow(
-             column(5, h3("2018"), 
-                    radioButtons(ns ("filingStatus_2018"), label= "1. Please select your filing status below:",
-                                 choices = c("Single", "Married Filing Jointly", "Married Filing Separately", "Head of Household", "Qualified Widower"), 
-                                 selected = "Single", inline = FALSE),
-                    "Need help? Visit IRS Interactive Tax Assistant: ",
-                    a(href = "https://www.irs.gov/help/ita/what-is-my-filing-status", "What is my filing status?")
-             ),
-             column(5, h3("2017"), 
-                    radioButtons(ns("filingStatus_2017"), label= "1. Please select your filing status below:",
-                                 choices = c("Single", "Married Filing Jointly", "Married Filing Separately", "Head of Household", "Qualified Widower"), 
-                                 selected = "Single", inline = FALSE),
-                    "Need help? Visit IRS Interactive Tax Assistant: ",
-                    a(href = "https://www.irs.gov/help/ita/what-is-my-filing-status", "What is my filing status?")
-             )
-           ), # End first row of filing status
-           hr(),
-           fluidRow(
-             column(5, 
-                    radioButtons(ns ("isDependent_2018"), label = "Can someone claim you as a dependent on their tax return?",
-                                 choices = c("Yes", "No"), selected = "No")
-             ), 
-             column(5, 
-                    radioButtons(ns ("isDependent_2017"), label = "Can someone claim you as a dependent on their tax return?",
-                                 choices = c("Yes", "No"), selected = "No") 
-             ) 
-           ), # End second row of checking dependency on other people return
-           hr(),
-           h3("Number of Dependents"),
-           fluidRow(
-             column(5, 
-                    numericInput(ns("numQualifiedChild_2018"), label = "Enter number of your qualifying child",
-                                 min= 0, max=10, value = 0),
-                    numericInput(ns("numQualifiedRelative_2018"), label = "Enter number of your qualifying relative",
-                                 min= 0, max=10, value = 0),
-                    "Need help? Visit this Turbo Tax article to learn more: ",
-                    a(href = "https://turbotax.intuit.com/tax-tips/family/rules-for-claiming-a-dependent-on-your-tax-return/L8LODbx94", 
-                      "Rules for claiming a dependent on your tax return")
-             ), 
-             column(5, 
-                    numericInput(ns("numQualifiedChild_2017"), label = "Enter number of your qualifying child",
-                                 min= 0, max=5, value = 0),
-                    numericInput(ns("numQualifiedRelative_2017"), label = "Enter number of your qualifying relative",
-                                 min= 0, max=5, value = 0),
-                    "Need help? Visit this Turbo Tax article to learn more: ",
-                    a(href = "https://turbotax.intuit.com/tax-tips/family/rules-for-claiming-a-dependent-on-your-tax-return/L8LODbx94", 
-                      "Rules for claiming a dependent on your tax return")
-             ) 
-           ),# Ending third row, entering number of dependents
-           hr(),
-           h3("Ages:"),
-           fluidRow(
-             column(5, 
-                    numericInput(ns("yourAge_2018"), label = "Enter your age:",
-                                 min= 0, max=200, value = 30),
-                    checkboxInput(ns("youBlind_2018"), label = "Check if you are blind", value = FALSE),
-                    numericInput(ns("spouseAge_2018"), label = "Enter your spouse age:",
-                                 min= 0, max=200, value = 30),
-                    checkboxInput(ns("spouseBlind_2018"), label = "Check if you are blind", value = FALSE),
-                    numericInput(ns("child1Age_2018"), label = "Enter your child 1 age:",
-                                 min = 0, max=200, value = 10),
-                    
-                    numericInput(ns("child2Age_2018"), label = "Enter your child 2 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns("child3Age_2018"), label = "Enter your child 3 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns("child4Age_2018"), label = "Enter your child 4 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "child5Age_2018"), label = "Enter your child 5 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "qualifiedRel1_2018"), label = "Enter your qualifying relative 1 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "qualifiedRel2_2018"), label = "Enter your qualifying relative 2 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "qualifiedRel3_2018"), label = "Enter your qualifying relative 3 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "qualifiedRel4_2018"), label = "Enter your qualifying relative 4 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "qualifiedRel5_2018"), label = "Enter your qualifying relative 5 age:",
-                                 min = 0, max=200, value = 10)
-             ), 
-             column(5, 
-                    numericInput(ns( "yourAge_2017"), label = "Enter your age:",
-                                 min= 0, max=200, value = 30),
-                    checkboxInput(ns("youBlind_2017"), label = "Check if you are blind", value = FALSE),
-                    numericInput(ns( "spouseAge_2017"), label = "Enter your spouse age:",
-                                 min= 0, max=200, value = 30),
-                    checkboxInput(ns("spouseBlind_2017"), label = "Check if you are blind", value = FALSE),
-                    numericInput(ns( "child1Age_2017"), label = "Enter your child 1 age:",
-                                 min = 0, max=200, value = 10),
-                    
-                    numericInput(ns( "child2Age_2017"), label = "Enter your child 2 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "child3Age_2017"), label = "Enter your child 3 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "child4Age_2017"), label = "Enter your child 4 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "child5Age_2017"), label = "Enter your child 5 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "qualifiedRel1_2017"), label = "Enter your qualifying relative 1 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "qualifiedRel2_2017"), label = "Enter your qualifying relative 2 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "qualifiedRel3_2017"), label = "Enter your qualifying relative 3 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "qualifiedRel4_2017"), label = "Enter your qualifying relative 4 age:",
-                                 min = 0, max=200, value = 10),
-                    numericInput(ns( "qualifiedRel5_2017"), label = "Enter your qualifying relative 5 age:",
-                                 min = 0, max=200, value = 10)
-             ) 
-           )
+     fluidRow(
+       column(5, h3("2018"), 
+              radioButtons(ns ("filingStatus_2018"), label= "1. Please select your filing status below:",
+                           choices = c("Single", "Married Filing Jointly", "Married Filing Separately", "Head of Household", "Qualified Widower"), 
+                           selected = "Single", inline = FALSE),
+              "Need help? Visit IRS Interactive Tax Assistant: ",
+              a(href = "https://www.irs.gov/help/ita/what-is-my-filing-status", "What is my filing status?")
+       ),
+       column(5, h3("2017"), 
+              radioButtons(ns("filingStatus_2017"), label= "1. Please select your filing status below:",
+                           choices = c("Single", "Married Filing Jointly", "Married Filing Separately", "Head of Household", "Qualified Widower"), 
+                           selected = "Single", inline = FALSE),
+              "Need help? Visit IRS Interactive Tax Assistant: ",
+              a(href = "https://www.irs.gov/help/ita/what-is-my-filing-status", "What is my filing status?")
+       )
+     ), # End first row of filing status
+     hr(),
+     fluidRow(
+       column(5, 
+              radioButtons(ns ("isDependent_2018"), label = "Can someone claim you as a dependent on their tax return?",
+                           choices = c("Yes", "No"), selected = "No")
+       ), 
+       column(5, 
+              radioButtons(ns ("isDependent_2017"), label = "Can someone claim you as a dependent on their tax return?",
+                           choices = c("Yes", "No"), selected = "No") 
+       ) 
+     ), # End second row of checking dependency on other people return
+     hr(),
+     h3("Number of Dependents"),
+     fluidRow(
+       column(5, 
+              numericInput(ns("numQualifiedChild_2018"), label = "Enter number of your qualifying child",
+                           min= 0, max=10, value = 0),
+              numericInput(ns("numQualifiedRelative_2018"), label = "Enter number of your qualifying relative",
+                           min= 0, max=10, value = 0),
+              "Need help? Visit this Turbo Tax article to learn more: ",
+              a(href = "https://turbotax.intuit.com/tax-tips/family/rules-for-claiming-a-dependent-on-your-tax-return/L8LODbx94", 
+                "Rules for claiming a dependent on your tax return")
+       ), 
+       column(5, 
+              numericInput(ns("numQualifiedChild_2017"), label = "Enter number of your qualifying child",
+                           min= 0, max=5, value = 0),
+              numericInput(ns("numQualifiedRelative_2017"), label = "Enter number of your qualifying relative",
+                           min= 0, max=5, value = 0),
+              "Need help? Visit this Turbo Tax article to learn more: ",
+              a(href = "https://turbotax.intuit.com/tax-tips/family/rules-for-claiming-a-dependent-on-your-tax-return/L8LODbx94", 
+                "Rules for claiming a dependent on your tax return")
+       ) 
+     ),# Ending third row, entering number of dependents
+     hr(),
+     h3("Ages:"),
+     fluidRow(
+       column(5, 
+              numericInput(ns("yourAge_2018"), label = "Enter your age:",
+                           min= 0, max=200, value = 30),
+              checkboxInput(ns("youBlind_2018"), label = "Check if you are blind", value = FALSE),
+              numericInput(ns("spouseAge_2018"), label = "Enter your spouse age:",
+                           min= 0, max=200, value = 0),
+              checkboxInput(ns("spouseBlind_2018"), label = "Check if you are blind", value = FALSE),
+              numericInput(ns("child1Age_2018"), label = "Enter your child 1 age:",
+                           min = 0, max=200, value = 0),
+              
+              numericInput(ns("child2Age_2018"), label = "Enter your child 2 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns("child3Age_2018"), label = "Enter your child 3 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns("child4Age_2018"), label = "Enter your child 4 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "child5Age_2018"), label = "Enter your child 5 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "qualifiedRel1_2018"), label = "Enter your qualifying relative 1 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "qualifiedRel2_2018"), label = "Enter your qualifying relative 2 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "qualifiedRel3_2018"), label = "Enter your qualifying relative 3 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "qualifiedRel4_2018"), label = "Enter your qualifying relative 4 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "qualifiedRel5_2018"), label = "Enter your qualifying relative 5 age:",
+                           min = 0, max=200, value = 0)
+       ), 
+       column(5, 
+              numericInput(ns( "yourAge_2017"), label = "Enter your age:",
+                           min= 0, max=200, value = 30),
+              checkboxInput(ns("youBlind_2017"), label = "Check if you are blind", value = FALSE),
+              numericInput(ns( "spouseAge_2017"), label = "Enter your spouse age:",
+                           min= 0, max=200, value = 0),
+              checkboxInput(ns("spouseBlind_2017"), label = "Check if you are blind", value = FALSE),
+              numericInput(ns( "child1Age_2017"), label = "Enter your child 1 age:",
+                           min = 0, max=200, value = 0),
+              
+              numericInput(ns( "child2Age_2017"), label = "Enter your child 2 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "child3Age_2017"), label = "Enter your child 3 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "child4Age_2017"), label = "Enter your child 4 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "child5Age_2017"), label = "Enter your child 5 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "qualifiedRel1_2017"), label = "Enter your qualifying relative 1 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "qualifiedRel2_2017"), label = "Enter your qualifying relative 2 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "qualifiedRel3_2017"), label = "Enter your qualifying relative 3 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "qualifiedRel4_2017"), label = "Enter your qualifying relative 4 age:",
+                           min = 0, max=200, value = 0),
+              numericInput(ns( "qualifiedRel5_2017"), label = "Enter your qualifying relative 5 age:",
+                           min = 0, max=200, value = 0)
+       ) 
+     )
   )
 }
 filingStatus <- function (input, output, session){
@@ -144,5 +144,50 @@ filingStatus <- function (input, output, session){
       show(id = "spouseAge_2017")
       show(id = "spouseBlind_2017")
     }
+  }) # Finish cosmetic issues.
+  filingStatusDF <- reactive ({
+    rowNames <- c("filingStatus", "dependent", "numQualifyingChild", "numQualifyingRelative",
+                  "yourAge", "spouseAge", "youBlind", "spouseBlind", "child1Age", "child2Age",
+                  "child3Age", "child4Age", "child5Age", "relative1Age", "relative2Age",
+                  "relative3Age", "relative4Age", "relative5Age")
+    status_2018 <- c( input$filingStatus_2018, 
+                      input$isDependent_2018,
+                      input$numQualifiedChild_2018,
+                      input$numQualifiedRelative_2018,
+                      input$yourAge_2018,
+                      input$spouseAge_2018,
+                      input$youBlind_2018,
+                      input$spouseBlind_2018,
+                      input$child1Age_2018,
+                      input$child2Age_2018,
+                      input$child3Age_2018,
+                      input$child4Age_2018,
+                      input$child5Age_2018,
+                      input$qualifiedRel1_2018,
+                      input$qualifiedRel2_2018,
+                      input$qualifiedRel3_2018,
+                      input$qualifiedRel4_2018,
+                      input$qualifiedRel5_2018
+                     )
+    status_2017 <- c( input$filingStatus_2017, 
+                      input$isDependent_2017,
+                      input$numQualifiedChild_2017,
+                      input$numQualifiedRelative_2017,
+                      input$yourAge_2017,
+                      input$spouseAge_2017,
+                      input$youBlind_2017,
+                      input$spouseBlind_2017,
+                      input$child1Age_2017,
+                      input$child2Age_2017,
+                      input$child3Age_2017,
+                      input$child4Age_2017,
+                      input$child5Age_2017,
+                      input$qualifiedRel1_2017,
+                      input$qualifiedRel2_2017,
+                      input$qualifiedRel3_2017,
+                      input$qualifiedRel4_2017,
+                      input$qualifiedRel5_2017)
+    return (data.frame(status_2018, status_2017, row.names = rowNames))
   })
+  
 }
