4cad607211b9edb12e9b8bfd3b866acc2e2d6246
longnguyen87 <nguyenhlongvn@gmail.com>
2019-03-17 21:59:30 -0500

    Change instructions page

--- Instructions.R
+++ Instructions.R
@@ -1,5 +1,8 @@
 instructionUI <- function (id){
 
   tabPanel("Instructions:",
-           p("Please refer to this ", a(href="https://adatascientist-cpa.com/wp-content/uploads/2019/03/R_TaxProject.pptx","User Guide"), " for instructions of how to use this app."))
+           p("Please refer to this ", a(href="https://adatascientist-cpa.com/wp-content/uploads/2019/03/R_TaxProject.pptx","User Guide"), " for instructions of how to use this app."),
+           p("Please start by selecting the ", strong('Filing Status & Dependency')," Tab on the right panel"),
+           tags$img(src = "TaxGraph.png",height = "600", width = "900")
+          )
 }
